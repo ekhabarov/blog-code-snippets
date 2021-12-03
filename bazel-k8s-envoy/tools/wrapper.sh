48e1420425cfcb332b769a4bcde290c061dd1251
Evgeny Khabarov <ekhabarov@dev.ms>
2021-12-03 13:42:30 -0500

    Fix wrapper.sh.

--- bazel-k8s-envoy/tools/wrapper.sh
+++ bazel-k8s-envoy/tools/wrapper.sh
@@ -6,7 +6,10 @@ TOOL=$1
 shift
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-   TOOL_PATH="external/${TOOL}_darwin_x86_64/${TOOL}"
+  realpath() {
+      [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+  }
+  TOOL_PATH="$(realpath "external/${TOOL}_darwin_x86_64/${TOOL}")"
 else
    TOOL_PATH="external/${TOOL}_linux_x86_64/${TOOL}"
 fi
